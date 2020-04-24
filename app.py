2c417c72bc7cfb0e4938b7746486fb7ad8975cea
Chetan Sinha <50909804+ChetanSinha@users.noreply.github.com>
2020-04-24 22:48:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from passlib.hash import sha256_crypt
 app = Flask(__name__)
 
 # Configure DataBase
-db = yaml.load(open('db.yaml'))
+db = yaml.load(open('db.yaml'), Loader=yaml.FullLoader)
+
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
@@ -28,30 +29,30 @@ def index():
     return render_template('index.html')
 
 # property options for buyer
-@app.route('/buyproperty', methods=['GET', 'POST'])
-def property():
+@app.route('/buyproperty/<int:id>', methods=['GET', 'POST'])
+def property(id):
     select = request.form
     if request.method == 'POST':
-        address = select['address']
-        type_ = select['type']
+        address = select['location']
+        # size = select['size']
 
         cur = mysql.connection.cursor()
 
-        result = cur.execute("SELECT * FROM property WHERE address=%s AND type=%s",(address, type_))
+        result = cur.execute('SELECT * FROM property WHERE location=%s',[address])
         
         if result > 0:
             data = cur.fetchall()
-            return render_template('propertylist.html', data=data)
+            return render_template('propertylist.html', data=data, id=id)
         else:
             return 'No match found'
         
         cur.close()
         
-    return render_template('propertylist.html', form=select)
+    return render_template('propertylist.html', form=select, id=id)
 
 # agent registration
 @app.route('/agentregister', methods=['GET', 'POST'])
-def register():
+def aregister():
     form = request.form
     if request.method == 'POST':
         
@@ -67,9 +68,28 @@ def register():
 
     return render_template('register.html', form=form)
 
+# agent registration
+@app.route('/buyerregister', methods=['GET', 'POST'])
+def bregister():
+    form = request.form
+    if request.method == 'POST':
+        
+        name = form['name']
+        password = sha256_crypt.encrypt(str(form['password']))
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO buyer(name, password) VALUES(%s, %s)", (name, password))
+        
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/')
+
+    return render_template('register.html', form=form)
+
+
 # agent's login
 @app.route('/agentlogin', methods=['GET', 'POST'])
-def login():
+def alogin():
     if request.method == 'POST':
         name = request.form['name']
         password_candidate = request.form['password']
@@ -106,10 +126,50 @@ def login():
 
 # agent's after login
 @app.route('/agentlogged')
-def dashboard():
+def adashboard():
     return render_template('agentdashboard.html')
 
-# sell property or adding to the list of properties
+# buyer login
+@app.route('/buyerlogin', methods=['GET', 'POST'])
+def blogin():
+    if request.method == 'POST':
+        name = request.form['name']
+        password_candidate = request.form['password']
+
+        cur = mysql.connection.cursor()
+        result = cur.execute("SELECT * FROM buyer WHERE name=%s", [name])
+
+        if result > 0:
+            # get the stored hash
+            flag=False
+            data = cur.fetchall()
+            for row in data:
+                password = row['password']
+                Id = row['buyer_id']
+                if sha256_crypt.verify(password_candidate, password):
+                    flag = True
+                    break
+            if flag:
+                session['logged_in'] = True
+                session['username'] = name
+                session['Id'] = Id
+                return redirect('/buyerlogged')
+            else:
+                return 'Wrong Password'
+        else:
+            # error = "Invalid login"
+            return render_template('login.html')
+        
+        cur.close()
+
+    return render_template('login.html')
+
+
+@app.route('/buyerlogged')
+def bdashboard():
+    return render_template('buyerdashboard.html')
+
+# adding to the list of properties
 @app.route('/sellProperty/<int:id>', methods=['GET', 'POST'])
 def sellproperty(id):
     property_type = request.form
@@ -118,12 +178,12 @@ def sellproperty(id):
         type_ = property_type['type']
         sp = property_type['selling_price']
         rp = property_type['renting_price']
-        avail = property_type['availability']
+        avail = property_type['availablity']
         dos = property_type['date_of_selling']
         agent_id = id
 
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO property(address, type, selling_price, rent_price, availability, date_of_selling, agent_id) VALUES(%s, %s, %s, %s, %s, %s, %s)",[add, type_, sp, rp, avail, dos, agent_id])
+        cur.execute("INSERT INTO property(address, type, selling_price, rent_price, availablity, date_of_selling, agent_id) VALUES(%s, %s, %s, %s, %s, %s, %s)",[add, type_, sp, rp, avail, dos, agent_id])
         mysql.connection.commit()
         cur.close()
         return redirect('/agentlogged')
@@ -131,10 +191,17 @@ def sellproperty(id):
     return render_template('addproperty.html', form=property_type)
 
 # agent's notification(not implemented)
-@app.route('/notification<int:id>')
-def notification():
-    pass
-    # return render_template('')
+@app.route('/notification/<int:agentid>')
+def agentnotification(agentid):
+    cur = mysql.connection.cursor()
+    result = cur.execute("SELECT * FROM property WHERE (availablity=%s OR availablity=%s) AND agent_id=%s AND date_of_selling = 'NA'",[-1, 1, agentid])
+    if result > 0:
+        data = cur.fetchall()
+        return render_template('agentnotifylist.html', data=data)
+    else:
+        return 'No Notifications'
+    return redirect('/agentlogged')
+        
 
 # agent logout
 @app.route('/logout')
@@ -142,45 +209,97 @@ def logout():
     session.clear()
     return redirect('/')
 
-# contact option for buyer(not fully implemented)
-@app.route('/contact/<string:address>')
-def contact(address):
+
+@app.route('/contact/<string:buytype>/<int:propid>/<int:buyid>')
+def contact(buytype, propid, buyid):
     cur = mysql.connection.cursor()
-    id = cur.execute("SELECT id FROM property WHERE address=%s",[address])
+    if buytype == 'rent':
+        cur.execute("UPDATE property SET buyer_id = %s, availablity = %s WHERE property_id = %s", [ buyid, -1, propid])
+        
+    elif buytype == 'buy':
+        cur.execute("UPDATE property SET buyer_id = %s, availablity = %s WHERE property_id = %s", [ buyid, 1, propid])
+
+    mysql.connection.commit()
     cur.close()
-    return redirect('/notification/id')
+    return redirect('/buyerlogged')
+
+@app.route('/agentproperty/<int:agentid>')
+def agentproperty(agentid):
+    cur = mysql.connection.cursor()
+    result = cur.execute('SELECT * FROM property WHERE agent_id=%s',[agentid])
+    if result > 0:
+        data = cur.fetchall()
+        return render_template('agentproplist.html', data=data)
+    else:
+        return 'No Property'
+    cur.close()
+    return redirect('/agentlogged')
+
+
+@app.route('/confirm/<int:propId>')
+def confirm(propId):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT availablity FROM property WHERE property_id = %s", [propId])
+    available_flag = cur.fetchone()
+    
+    # SELL 
+    if available_flag == 1:
+        cur.execute("UPDATE property SET availablity = %s, date_of_selling = %s WHERE property_id = %s AND buyer_id <> -1", [1, get_date(), propId])
+
+    # RENT
+    elif available_flag == -1:
+        cur.execute("UPDATE property SET availablity = %s, date_of_selling = %s WHERE property_id = %s AND buyer_id <> -1",[-1, get_date(), propId ])
+    mysql.connection.commit()
+    cur.close()
+    return redirect('/agentlogged')
+
 
 # more information of the property(not implemented)
 @app.route('/moreinfo<string:address>')
 def moreinfo(address):
     pass
 
-# @app.route('/delete/<string:address>')
-# def delete(address):
-#     # task_to_delete = Todo.query.get_or_404(id)
-#     try:
-#         cur = mysql.connection.cursor()
-#         cur.execute("DELETE FROM property WHERE address=%s", [address])
-#         mysql.connection.commit()
-#         cur.close()
-#         return redirect('/')
-#     except:
-#         return "Problem in deletion"
+
+# __________________________________________________________________________________________________________________________________
+# REAL ESATATE AGENT OFFICE
+
+@app.route('/reoffice')
+def reoffice():
+    cur = mysql.connection.cursor()
+    sell_result = cur.execute("SELECT agent.agent_id, name, SUM(selling_price) as sum FROM agent, property WHERE availablity=1 AND agent.agent_id=property.agent_id AND buyer_id <> -1 GROUP BY property.agent_id")
+    
+    sell_data = cur.fetchall()
+    rent_result = cur.execute("SELECT agent.agent_id, name, SUM(renting_price) as sum FROM agent, property WHERE availablity=-1 AND agent.agent_id=property.agent_id AND buyer_id <> -1 GROUP BY property.agent_id")
+    
+    rent_data = cur.fetchall()
+
+    if sell_result + rent_result > 0:
+        return render_template('agentreport.html', sell_data=sell_data, rent_data=rent_data)
+    
+    else:
+        return 'No Selling'
+    
+    cur.close()
+    return redirect('/')
 
 
-# @app.route('/update/<string:address>', methods=['GET', 'POST'])
-# def update(address):
-#     # task = Todo.query.get_or_404(id)
-#     if request.method == 'POST':
-#         task.content = request.form['content']
-
-#         try:
-#             db.session.commit()
-#             return redirect('/')
-#         except:
-#             return 'update issue'
-#     else:
-#         return render_template('update.html', task=task)
+@app.route('/sellingdetails/<string:buytype>/<int:agentid>')
+def sellingdetails(buytype, agentid):
+    cur = mysql.connection.cursor()
+    if buytype == 'rented':
+        result = cur.execute("SELECT location, type, size, date_of_entry, date_of_selling, renting_price, buyer_id FROM property WHERE agent_id=%s AND availablity=-1", [agentid])
+        if result > 0:
+            data = cur.fetchall()
+            return render_template('rentingreport.html', data=data)
+    
+    elif buytype == 'sold':
+        result = cur.execute("SELECT location, type, size, date_of_entry, date_of_selling, selling_price, buyer_id FROM property WHERE agent_id=%s AND availablity=1", [agentid])
+        if result > 0:
+            data = cur.fetchall()
+            return render_template('sellingreport.html', data=data)
+    
+    cur.close()
+    return redirect('/')
 
 if __name__ == '__main__':
     app.secret_key='secret123'
