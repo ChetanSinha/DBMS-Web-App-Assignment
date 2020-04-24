1d6e15c22a7d999544db0333d5491173d175d54a
Chetan Sinha <50909804+ChetanSinha@users.noreply.github.com>
2020-04-24 23:20:52 +0530

    Add files via upload

---
+++ property.sql
@@ -0,0 +1,62 @@
+CREATE TABLE property(
+   location        VARCHAR(100) NOT NULL PRIMARY KEY
+  ,type            VARCHAR(100) NOT NULL
+  ,availablity     BIT  NOT NULL
+  ,size            VARCHAR(24) NOT NULL
+  ,selling_price   NUMERIC(5,2) NOT NULL
+  ,renting_price   NUMERIC(5,2) NOT NULL
+  ,agent_id        INTEGER  NOT NULL
+  ,date_of_entry   VARCHAR(100) NOT NULL
+  ,date_of_selling VARCHAR(100) NOT NULL
+  ,buyer_id        INTEGER  NOT NULL
+);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Electronic City Phase II','Super built-up  Area',0,'2 BHK',39.07,39.07,3,'2014-01-25','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Chikka Tirupathi','Plot  Area',0,'4 Bedroom',120,120,1,'2014-05-24','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Uttarahalli','Built-up  Area',0,'3 BHK',62,62,1,'2013-12-25','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Lingadheeranahalli','Super built-up  Area',0,'3 BHK',95,95,3,'2012-10-07','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Kothanur','Super built-up  Area',0,'2 BHK',51,51,6,'2014-02-08','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Whitefield','Super built-up  Area',0,'2 BHK',38,38,3,'2013-10-15','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Old Airport Road','Super built-up  Area',0,'4 BHK',204,204,1,'2014-08-18','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Rajaji Nagar','Super built-up  Area',0,'4 BHK',600,600,3,'2013-07-11','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Marathahalli','Super built-up  Area',0,'3 BHK',63.25,63.25,2,'2012-02-20','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Gandhi Bazar','Plot  Area',0,'6 Bedroom',370,370,7,'2012-05-26','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Whitefield','Super built-up  Area',0,'3 BHK',70,70,2,'2014-06-04','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Whitefield','Plot  Area',0,'4 Bedroom',295,295,2,'2012-11-04','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('7th Phase JP Nagar','Super built-up  Area',0,'2 BHK',38,38,5,'2014-07-09','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Gottigere','Built-up  Area',0,'2 BHK',40,40,6,'2012-02-02','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Sarjapur','Plot  Area',0,'3 Bedroom',148,148,2,'2012-10-04','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Mysore Road','Super built-up  Area',0,'2 BHK',73.5,73.5,7,'2014-08-24','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Bisuvanahalli','Super built-up  Area',0,'3 BHK',48,48,7,'2012-10-12','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Raja Rajeshwari Nagar','Super built-up  Area',0,'3 BHK',60,60,6,'2012-04-05','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Ramakrishnappa Layout','Super built-up  Area',0,'3 BHK',290,290,1,'2013-12-14','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Manayata Tech Park','Super built-up  Area',0,'2 BHK',48,48,3,'2014-01-26','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Kengeri','Built-up  Area',0,'1 BHK',15,15,6,'2015-05-13','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Binny Pete','Super built-up  Area',0,'3 BHK',122,122,3,'2017-12-29','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Thanisandra','Plot  Area',0,'4 Bedroom',380,380,2,'2017-07-19','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Bellandur','Super built-up  Area',0,'3 BHK',103,103,5,'2017-12-21','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Thanisandra','Super built-up  Area',0,'1 RK',25.25,25.25,3,'2018-07-29','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Mangammanapalya','Super built-up  Area',0,'3 BHK',56,56,6,'2015-04-10','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Electronic City','Super built-up  Area',0,'2 BHK',23.1,23.1,2,'2017-12-18','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Whitefield','Built-up  Area',0,'3 BHK',81,81,7,'2015-05-13','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Ramagondanahalli','Super built-up  Area',0,'2 BHK',48.77,48.77,3,'2016-10-09','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Electronic City','Super built-up  Area',0,'3 BHK',47,47,4,'2017-10-27','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Yelahanka','Super built-up  Area',0,'4 BHK',186,186,3,'2016-12-13','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Bisuvanahalli','Super built-up  Area',0,'3 BHK',35,35,1,'2017-07-31','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Hebbal','Super built-up  Area',0,'3 BHK',123,123,1,'2015-08-21','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Raja Rajeshwari Nagar','Super built-up  Area',0,'3 BHK',57.39,57.39,1,'2017-02-16','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Kasturi Nagar','Built-up  Area',0,'3 BHK',125,125,3,'2016-02-05','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Kanakpura Road','Super built-up  Area',0,'2 BHK',36,36,7,'2018-09-27','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Electronics City Phase 1','Super built-up  Area',0,'2 BHK',45.5,45.5,7,'2018-10-27','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Kundalahalli','Super built-up  Area',0,'3 BHK',125,125,1,'2016-09-11','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Chikkalasandra','Super built-up  Area',0,'3 BHK',56.12,56.12,4,'2017-03-11','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Uttarahalli','Built-up  Area',0,'2 BHK',45,45,7,'2018-08-17','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Murugeshpalya','Built-up  Area',0,'2 BHK',81,81,6,'2019-02-15','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Sarjapur  Road','Super built-up  Area',0,'3 BHK',38,38,1,'2019-04-22','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Ganga Nagar','Super built-up  Area',0,'1 BHK',38,38,1,'2019-10-06','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Yelahanka','Plot  Area',0,'1 Bedroom',48,48,3,'2019-06-20','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Kanakpura Road','Super built-up  Area',0,'2 BHK',91.79,91.79,7,'2019-04-10','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('HSR Layout','Plot  Area',0,'8 Bedroom',200,200,5,'2019-11-27','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Doddathoguru','Super built-up  Area',0,'2 BHK',33,33,7,'2019-10-24','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Whitefield','Super built-up  Area',0,'2 BHK',94.82,94.82,1,'2019-11-13','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('KR Puram','Plot  Area',0,'2 Bedroom',130,130,1,'2019-10-02','NA',-1);
+INSERT INTO property(location,type,availablity,size,selling_price,renting_price,agent_id,date_of_entry,date_of_selling,buyer_id) VALUES ('Himagiri Meadows','Built-up  Area',0,'2 BHK',36,36,2,'2019-05-10','NA',-1);
